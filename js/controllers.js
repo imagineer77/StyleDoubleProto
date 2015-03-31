73cbac1445d12c613db6a4f4d18479af416f2917
Gary Knopp <geknopp@gmail.com>
2015-03-31 15:23:14 -0700

    Finished up prototype

--- js/controllers.js
+++ js/controllers.js
@@ -34,6 +34,22 @@ angular.module('styledouble.controllers', [])
         DataStore.weight = value;
     };
 
+    DataStore.setNeck = function (value) {
+        DataStore.neck = value;
+    };
+
+    DataStore.setChest = function (value) {
+        DataStore.chest = value;
+    };
+
+    DataStore.setWaist = function (value) {
+        DataStore.waist = value;
+    };
+
+    DataStore.setHips = function (value) {
+        DataStore.hips = value;
+    };
+
     return DataStore;
 })
 
@@ -61,17 +77,6 @@ angular.module('styledouble.controllers', [])
 })
 
 .controller('MeasurementsCtrl', function($scope, $state, DataStore) {
-    $scope.formData ={};
-    $scope.next = function() {
-        //this.setValues();
-        $state.go('tab.my-sizes');
-    };
-
-    $scope.details = function() {
-        this.setValues();
-        $state.go('tab.measurement-details');
-    };
-
     $scope.setValues = function() {
         alert("Clicked: " + $scope.formData.neck + ":" + $scope.formData.chest
         + ":" + $scope.formData.waist + ":" + $scope.formData.hips);
@@ -80,13 +85,24 @@ angular.module('styledouble.controllers', [])
         DataStore.setWaist($scope.formData.waist);
         DataStore.setHips($scope.formData.hips);
     };
+
+    $scope.formData ={};
+    $scope.next = function() {
+        this.setValues();
+        $state.go('tab.my-sizes');
+    };
+
+    $scope.details = function() {
+        this.setValues();
+        $state.go('tab.measurement-details');
+    };
 })
 
 .controller('MeasurementDetailsCtrl', function($scope, $state, DataStore) {
     $scope.formData ={};
     $scope.next = function() {
         alert("Clicked: " );
-        // Datastore set values here
+        // Datastore set values here???
         $state.go('tab.my-sizes');
     };
 })
@@ -110,10 +126,21 @@ angular.module('styledouble.controllers', [])
 })
 
 .controller('AccountCtrl', function($scope, $state, DataStore) {
-    $scope.formData ={};
-    $scope.finish = function() {
-        alert("Clicked: " );
-        // Datastore set values
-        // Create account
-    };
+        $scope.email = 'john.doe@gmail.com';
+        $scope.password = 'password';
+        $scope.passwordx = 'verify';
+        $scope.formData ={};
+        $scope.finish = function() {
+            alert("Email: " + $scope.formData.email + "\n"
+                    + "Password: " + $scope.formData.password + "\n"
+                    + "Password Verify "+ $scope.formData.passwordx + "\n"
+                    + "Gender: " + DataStore.gender + "\n"
+                    + "Birth year: " + + DataStore.birthyear + "\n"
+                    + "Height: " + + DataStore.heightfeet +" ft "+ DataStore.heightinches + " in" + "\n"
+                    + "Weight: " + + DataStore.weight + "\n"
+                    + "Neck: " + + DataStore.neck + " in" + "\n"
+                    + "Chest: " + + DataStore.chest + " in" + "\n"
+                    + "Waist: " + + DataStore.waist + " in" + "\n"
+                    + "Hips: " + + DataStore.hips + " in");
+        };
 });
