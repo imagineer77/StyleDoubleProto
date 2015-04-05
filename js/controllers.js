66a97ccf3f04e7f2a1cd86a47367b16b57644a1f
imagineer77 <imagineer77@users.noreply.github.com>
2015-04-05 10:35:11 -0700

    add hover over (to show more text / desc) but this will likely not working on touch screen

--- js/controllers.js
+++ js/controllers.js
@@ -165,6 +165,42 @@ angular.module('styledouble.controllers', [])
           $state.go('tab.gender');
         }
     };
+
+// http://ionicframework.com/docs/api/service/$ionicPopover/
+/*
+  // .fromTemplate() method
+  var template = '<ion-popover-view><ion-header-bar> <h1 class="title">My Popover Title</h1> </ion-header-bar> <ion-content> Hello! </ion-content></ion-popover-view>';
+
+  $scope.popover = $ionicPopover.fromTemplate(template, {
+    scope: $scope
+  });
+
+  // .fromTemplateUrl() method
+  $ionicPopover.fromTemplateUrl('my-popover.html', {
+    scope: $scope
+  }).then(function(popover) {
+    $scope.popover = popover;
+  });
+
+  $scope.openPopover = function($event) {
+    $scope.popover.show($event);
+  };
+  $scope.closePopover = function() {
+    $scope.popover.hide();
+  };
+  //Cleanup the popover when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.popover.remove();
+  });
+  // Execute action on hide popover
+  $scope.$on('popover.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove popover
+  $scope.$on('popover.removed', function() {
+    // Execute action
+  });
+*/
 })
 
 .controller('MeasurementsCtrl', function($scope, $state, DataStore) {
