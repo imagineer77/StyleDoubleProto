87f01aefa91351ac416d46fc00776b5705030c6d
Gary Knopp <geknopp@gmail.com>
2015-04-07 15:01:30 -0700

    New UI approach

--- js/app.js
+++ js/app.js
@@ -1,160 +1,200 @@
-angular.module('styledouble', ['ionic', 'styledouble.controllers'])
+angular.module('styledouble', ['ionic', 'styledouble.controllers', 'styledouble.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-.config(function($stateProvider, $urlRouterProvider) {
+    .config(function($stateProvider, $urlRouterProvider) {
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+            // setup an abstract state for the tabs directive
+            .state('tab', {
+                url: "/tab",
+                abstract: true,
+                templateUrl: "templates/tabs.html"
+            })
 
-  // Each tab has its own nav history stack:
+            // Each tab has its own nav history stack:
+            .state('tab.dash', {
+                url: '/dash',
+                views: {
+                    'tab-dash': {
+                        templateUrl: 'templates/tab-dash.html',
+                        controller: 'DashCtrl'
+                    }
+                }
+            })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+            .state('tab.doubles', {
+                url: '/doubles',
+                views: {
+                    'doubles': {
+                        templateUrl: 'templates/doubles.html',
+                        controller: 'DoublesCtrl'
+                    }
+                }
+            })
 
-  .state('tab.gender', {
-      url: "/gender",
-      views: {
-          'gender': {
-              templateUrl: 'templates/gender.html',
-              controller: 'GenderCtrl'
-          }
-      }
-  })
+            .state('tab.doubles-detail', {
+                url: '/doubles/:doubleId',
+                views: {
+                    'doubles': {
+                        templateUrl: 'templates/double-detail.html',
+                        controller: 'DoubleDetailCtrl'
+                    }
+                }
+            })
 
-  .state('tab.height-weight', {
-      url: '/height-weight',
-      views: {
-          'height-weight': {
-              templateUrl: 'templates/height-weight.html',
-              controller: 'HeightWeightCtrl'
-          }
-      }
-  })
+            .state('tab.recommendations', {
+                url: '/recommendations',
+                views: {
+                    'recommendations': {
+                        templateUrl: 'templates/recommendations.html',
+                        controller: 'RecommendationsCtrl'
+                    }
+                }
+            })
 
-  .state('tab.body-shape-female', {
-      url: "/body-shape-female",
-      views: {
-<<<<<<< HEAD
-          'body-shape': {
-=======
-          'body-shape-female': {
->>>>>>> 22db6b78b84f1641a554e7fc1f532df3f4bebb2a
-              templateUrl: 'templates/body-shape-female.html',
-              controller: 'BodyShapeFemaleCtrl'
-          }
-      }
-  })
+            .state('tab.recommendations-detail', {
+                url: '/recommendations/:recommendationId',
+                views: {
+                    'recommendations': {
+                        templateUrl: 'templates/recommendation-detail.html',
+                        controller: 'RecommendationDetailCtrl'
+                    }
+                }
+            })
 
-<<<<<<< HEAD
+            .state('tab.upload', {
+                url: '/upload',
+                views: {
+                    'upload': {
+                        templateUrl: 'templates/upload.html',
+                        controller: 'UploadCtrl'
+                    }
+                }
+            });
 
-  .state('tab.body-shape-male', {
-      url: "/body-shape-male",
-      views: {
-          'body-shape': {
-=======
-  .state('tab.body-shape-male', {
-      url: "/body-shape-male",
-      views: {
-          'body-shape-male': {
->>>>>>> 22db6b78b84f1641a554e7fc1f532df3f4bebb2a
-              templateUrl: 'templates/body-shape-male.html',
-              controller: 'BodyShapeMaleCtrl'
-          }
-      }
-  })
+/*
+        .state('tab.profile', {
+            url: '/profile',
+            views: {
+                'doubles': {
+                    templateUrl: 'templates/profile.html',
+                    controller: 'ProfileCtrl'
+                }
+            }
+        })
 
-  .state('tab.measurements', {
-      url: '/measurements',
-      views: {
-          'measurements': {
-              templateUrl: 'templates/measurements.html',
-              controller: 'MeasurementsCtrl'
-          }
-      }
-  })
+            .state('tab.gender', {
+                url: "/gender",
+                views: {
+                    'gender': {
+                        templateUrl: 'templates/gender.html',
+                        controller: 'GenderCtrl'
+                    }
+                }
+            })
 
-  .state('tab.measurement-details', {
-      url: '/measurement-details',
-      views: {
-          'measurement-details': {
-              templateUrl: 'templates/measurement-details.html',
-              controller: 'MeasurementDetailsCtrl'
-          }
-      }
-  })
+            .state('tab.height-weight', {
+                url: '/height-weight',
+                views: {
+                    'height-weight': {
+                        templateUrl: 'templates/height-weight.html',
+                        controller: 'HeightWeightCtrl'
+                    }
+                }
+            })
 
-  .state('tab.my-sizes', {
-      url: '/my-sizes',
-      views: {
-          'my-sizes': {
-              templateUrl: 'templates/my-sizes.html',
-              controller: 'MySizesCtrl'
-          }
-      }
-  })
+            .state('tab.body-shape-female', {
+                url: "/body-shape-female",
+                views: {
+                    'body-shape': {
+                        templateUrl: 'templates/body-shape-female.html',
+                        controller: 'BodyShapeFemaleCtrl'
+                    }
+                }
+            })
 
-  .state('tab.brands-like', {
-      url: '/brands-like',
-      views: {
-          'brands-like': {
-              templateUrl: 'templates/brands-like.html',
-              controller: 'BrandsLikeCtrl'
-          }
-      }
-  })
 
-  .state('tab.cloth-returns', {
-      url: '/cloth-returns',
-      views: {
-          'cloth-returns': {
-              templateUrl: 'templates/cloth-returns.html',
-              controller: 'ClothReturnsCtrl'
-          }
-      }
-  })
+            .state('tab.body-shape-male', {
+                url: "/body-shape-male",
+                views: {
+                    'body-shape': {
+                        templateUrl: 'templates/body-shape-male.html',
+                        controller: 'BodyShapeMaleCtrl'
+                    }
+                }
+            })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'account': {
-        templateUrl: 'templates/account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+            .state('tab.measurements', {
+                url: '/measurements',
+                views: {
+                    'measurements': {
+                        templateUrl: 'templates/measurements.html',
+                        controller: 'MeasurementsCtrl'
+                    }
+                }
+            })
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+            .state('tab.measurement-details', {
+                url: '/measurement-details',
+                views: {
+                    'measurement-details': {
+                        templateUrl: 'templates/measurement-details.html',
+                        controller: 'MeasurementDetailsCtrl'
+                    }
+                }
+            })
 
-});
+            .state('tab.my-sizes', {
+                url: '/my-sizes',
+                views: {
+                    'my-sizes': {
+                        templateUrl: 'templates/my-sizes.html',
+                        controller: 'MySizesCtrl'
+                    }
+                }
+            })
+
+            .state('tab.cloth-returns', {
+                url: '/cloth-returns',
+                views: {
+                    'cloth-returns': {
+                        templateUrl: 'templates/cloth-returns.html',
+                        controller: 'ClothReturnsCtrl'
+                    }
+                }
+            })
+
+            .state('tab.account', {
+                url: '/account',
+                views: {
+                    'account': {
+                        templateUrl: 'templates/account.html',
+                        controller: 'AccountCtrl'
+                    }
+                }
+            });
+*/
+
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/tab/dash');
+
+    });
--- js/services.js
+++ js/services.js
@@ -1,7 +1,105 @@
 angular.module('styledouble.services', [])
 
+.factory('Doubles', function() {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var doubles = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+        id: 2,
+        name: 'Andrew Jostlin',
+        lastText: 'Did you get the ice cream?',
+        face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+    }, {
+        id: 3,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+        id: 4,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    }];
+
+    return {
+        all: function() {
+            return doubles;
+        },
+        remove: function(double) {
+            doubles.splice(doubles.indexOf(double), 1);
+        },
+        get: function(doubleId) {
+            for (var i = 0; i < doubles.length; i++) {
+                if (doubles[i].id === parseInt(doubleId)) {
+                    return doubles[i];
+                }
+            }
+            return null;
+        }
+    }
+})
+
+.factory('Recommendations', function() {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var recommendations = [{
+        id: 0,
+        name: 'Ben Sparrow',
+        lastText: 'You on your way?',
+        face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+        id: 1,
+        name: 'Max Lynx',
+        lastText: 'Hey, it\'s me',
+        face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+        id: 2,
+        name: 'Andrew Jostlin',
+        lastText: 'Did you get the ice cream?',
+        face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
+    }, {
+        id: 3,
+        name: 'Adam Bradleyson',
+        lastText: 'I should buy a boat',
+        face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+        id: 4,
+        name: 'Perry Governor',
+        lastText: 'Look at my mukluks!',
+        face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
+    }];
+
+    return {
+        all: function() {
+            return recommendations;
+        },
+        remove: function(recommendation) {
+            recommendations.splice(recommendations.indexOf(recommendation), 1);
+        },
+        get: function(recommendationId) {
+            for (var i = 0; i < recommendations.length; i++) {
+                if (recommendations[i].id === parseInt(recommendationId)) {
+                    return recommendations[i];
+                }
+            }
+            return null;
+        }
+    }
+})
+
 .factory('DataStore', function() {
-   /* //create datastore with default values
+   //create datastore with default values
     var DataStore = {
         gender:       'male',
         birthyear:   '1950'
@@ -15,5 +113,5 @@ angular.module('styledouble.services', [])
         DataStore.birthyear = value;
     };
 
-    return DataStore;*/
+    return DataStore;
 });
